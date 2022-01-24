226a2fffcf1c6023c3f63a19390daa8f9506e55a
ROMCruise <45038867+dietschc@users.noreply.github.com>
2022-01-24 00:16:05 -0600

    Update create-tables.js

--- database/create-tables.js
+++ database/create-tables.js
@@ -11,21 +11,21 @@ var sql = '';
 connection.connect();
 
 sql = 'DROP TABLE IF EXISTS customers';
-connection.query(sql , function (error, results, fields) {
+connection.query(sql , function (error, results) {
   if (error) throw error;
-  console.log('DROP customer table: ');
+  console.log('DROP customer table: ', results);
 });
 
 sql = 'CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))';
-connection.query(sql, function (error, results, fields) {
+connection.query(sql, function (error, results) {
   if (error) throw error;
-  console.log('CREATE customer table: ');
+  console.log('CREATE customer table: ', results);
 });
 
 sql = 'INSERT INTO customers (name, address) VALUES ("Mike","432 Some Street")';
-connection.query(sql, function (error, results, fields) {
+connection.query(sql, function (error, results) {
   if (error) throw error;
-  console.log('INSERT INTO customers table: ');
+  console.log('INSERT INTO customers table: ', results);
 });
 
 
