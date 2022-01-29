fa0dc7981de05e1b43aca796cdd754586860b050
ROMCruise <45038867+dietschc@users.noreply.github.com>
2022-01-29 17:56:48 -0600

    Update Footer.js

--- frontend/src/components/views/Footer.js
+++ frontend/src/components/views/Footer.js
@@ -1,22 +1,27 @@
 // Initially Created by: Devin Brueberg
 // CSC450 Capstone
-// Restaurant Club - Chat.js
+// Restaurant Club - Footer.js
 // January 24, 2022
 // Last Edited (Initials, Date, Edits):
+// (CPD, 1/29/22, Style footer and setup getYear variable)
 
 // Using React library in order to build components 
 // for the app and importing needed components
 import React from 'react';
-import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const currentYear = new Date().getFullYear();
 
 function Footer(props) {
     return (
-        <div className="footer">
-            <h1>
-                Welcome to the Footer!
-            </h1>
-            <Link to="/">Back to Home</Link>
-        </div>
+        <footer className="fixed-bottom footer mt-auto py-3 bg-light text-center">
+            <div class="container">
+                <ul className="list-unstyled text-muted">
+                    <li>CSC 450 Group 3 Spring 2022</li>
+                    <li>Updated @{currentYear}</li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
