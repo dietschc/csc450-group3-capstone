7d324a65bf6437d9a65cb658c52562eb89911964
ROMCruise <45038867+dietschc@users.noreply.github.com>
2022-01-31 21:48:36 -0600

    Completed initial version of editAccount view

--- frontend/src/components/views/EditAccount.js
+++ frontend/src/components/views/EditAccount.js
@@ -3,30 +3,29 @@
 // Restaurant Club - EditAccount.js
 // January 24, 2022
 // Last Edited (Initials, Date, Edits):
-// (CPD, 1/29/22, GitHub#29)
+// (CPD, 1/29/22, GitHub-#29-EditAccount View Layout)
 
 // Using React library in order to build components 
 // for the app and importing needed components
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Row, Form, Container, Button } from 'react-bootstrap';
+import { Row, Col, Form, Container, Button } from 'react-bootstrap';
 
 function EditAccount(props) {
     let editing = false
-    let initialAccountState = ""
 
-    const [account, setAccount] = useState(initialAccountState)
+    // keeps track of if the form was submitted
+    const [submitted, setSubmitted] = useState(false)
+
     const [userName, setUserName] = useState("");
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
+    const [address, setAddress] = useState("");
+    const [city, setCity] = useState("");
     const [zip, setZip] = useState("");
     const [state, setState] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
-    // keeps track if account is submitted
-    const [submitted, setSubmitted] = useState(false)
-
 
     const onChangeUserName = e => {
         const userName = e.target.value
@@ -41,6 +40,16 @@ function EditAccount(props) {
     const onChangeLastName = e => {
         const lastName = e.target.value
         setLastName(lastName);
+    }
+
+    const onChangeAddress = e => {
+        const address = e.target.value
+        setAddress(address);
+    }
+
+    const onChangeCity = e => {
+        const city = e.target.value
+        setCity(city);
     }
 
     const onChangeZip = e => {
@@ -73,115 +82,167 @@ function EditAccount(props) {
             email: email,
             password: password
         }
+        console.log(data)
+        setSubmitted(true)
     }
 
     const clearForm = () => {
         setUserName("");
         setFirstName("");
         setLastName("");
+        setAddress("");
+        setCity("");
         setZip("");
         setState("");
         setEmail("");
         setPassword("");
+        console.log("Form cleared")
     }
 
     return (
-        <Container fluid>
-            {submitted ? (
-                <div className="text-center">
-                    <h4>Account information submitted successfully</h4>
-                    <Link to={"/"}>
-                        Back to Dashboard
-                    </Link>
+        <Container className="text-muted">
+            <Container as="header">
+                <div className="text-center p-1">
+                    <h1>{editing ? "Edit" : "Create"} Account</h1>
                 </div>
-
-            ) : (
-                <Form className="p-3">
+            </Container>
+            <Container fluid as="main" className="p-4">
+                {submitted ? (
                     <div className="text-center">
-                        <h4>{editing ? "Edit" : "Create"} Account</h4>
+                        <h4>Account information submitted successfully</h4>
+                        <Link to={"/"}>
+                            Back to Dashboard
+                        </Link>
                     </div>
 
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center pt-2">
-                        <Form.Label column sm={2}>User Name:</Form.Label>
-                        <Form.Control
-                            className="w-50"
-                            type="text"
-                            required
-                            value={userName}
-                            onChange={onChangeUserName}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center">
-                        <Form.Label column sm={2}>First Name:</Form.Label>
-                        <Form.Control
-                            className="w-50"
-                            type="text"
-                            required
-                            value={firstName}
-                            onChange={onChangeFirstName}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center">
-                        <Form.Label column sm={2}>Last Name:</Form.Label>
-                        <Form.Control
-                            className="w-50"
-                            type="text"
-                            required
-                            value={lastName}
-                            onChange={onChangeLastName}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center">
-                        <Form.Label column sm={2}>Zip:</Form.Label>
-                        <Form.Control
-                            className="w-50"
-                            type="text"
-                            required
-                            value={zip}
-                            onChange={onChangeZip}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center">
-                        <Form.Label column sm={2}>State:</Form.Label>
-                        <Form.Select className="w-50" aria-label="select state options">
-                            <option>Select State</option>
-                            <option value="1">Minnesota</option>
-                            <option value="2">Wisconsin</option>
-                            <option value="3">Somewhere else</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center">
-                        <Form.Label column sm={2}>Email:</Form.Label>
-                        <Form.Control
-                            className="w-50"
-                            type="text"
-                            required
-                            value={email}
-                            onChange={onChangeEmail}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Row} className="mb-3 justify-content-md-center">
-                        <Form.Label column sm={2}>Password:</Form.Label>
-                        <Form.Control
-                            className="w-50"
-                            type="text"
-                            required
-                            value={password}
-                            onChange={onChangePassword}
-                        />
-                    </Form.Group>
+                ) : (
+                    <Form>
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center pt-2">
+                            <Form.Label column sm={2}>User Name:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={userName}
+                                    onChange={onChangeUserName}
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                            <Form.Label column sm={2}>First Name:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={firstName}
+                                    onChange={onChangeFirstName}
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                            <Form.Label column sm={2}>Last Name:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={lastName}
+                                    onChange={onChangeLastName}
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                            <Form.Label column sm={2}>Address:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={address}
+                                    onChange={onChangeAddress}
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                            <Form.Label column sm={2}>City:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={city}
+                                    onChange={onChangeCity}
+                                />
+                            </Col>
+                        </Form.Group>
 
-                    <div className="d-flex justify-content-around pt-4">
-                        <Button variant="outline-primary" onClick={saveAccount}>
-                            {editing ? "Update" : "Submit"}
-                        </Button>
+                        <Row>
+                            <Col>
+                                <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                                    <Form.Label column>Zip:</Form.Label>
+                                    <Col sm={8}>
+                                        <Form.Control
+                                            type="text"
+                                            required
+                                            value={zip}
+                                            onChange={onChangeZip}
+                                        />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                                    <Form.Label column sm={2}>State:</Form.Label>
+                                    <Col sm={10}>
+                                        <Form.Select
+                                            aria-label="select state options"
+                                            value={state}
+                                            onChange={onChangeState}
+                                        >
+                                            <option>Select State</option>
+                                            <option value="MN">MN</option>
+                                            <option value="WI">WI</option>
+                                            <option value="XX">XX</option>
+                                        </Form.Select>
+                                    </Col>
+                                </Form.Group>
+                            </Col>
 
-                        <Button variant="outline-primary" onClick={clearForm}>
-                            Clear
-                        </Button>
-                    </div>
-                </Form>
-            )}
+
+                        </Row>
+
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                            <Form.Label column sm={2}>Email:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={email}
+                                    onChange={onChangeEmail}
+                                />
+                            </Col>
+                        </Form.Group>
+                        <Form.Group as={Row} className="mb-3 justify-content-md-center">
+                            <Form.Label column sm={2}>Password:</Form.Label>
+                            <Col sm={10}>
+                                <Form.Control
+                                    type="text"
+                                    required
+                                    value={password}
+                                    onChange={onChangePassword}
+                                />
+                            </Col>
+                        </Form.Group>
+
+                        <div className="d-flex justify-content-around pt-4">
+                            <Button variant="outline-primary" onClick={saveAccount}>
+                                {editing ? "Update" : "Submit"}
+                            </Button>
+
+                            <Button variant="outline-primary" onClick={clearForm}>
+                                Clear
+                            </Button>
+                        </div>
+                    </Form>
+                )}
+            </Container>
         </Container>
     )
 }
