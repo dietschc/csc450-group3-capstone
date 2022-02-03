a05c1d8bf3b790454e5a250dfc97722159d6643f
Devin Brueberg <brueberd@csp.edu>
2022-02-03 11:36:48 -0600

    Initial File Structure

---
+++ frontend/src/constants.js
@@ -0,0 +1,19 @@
+// Devin Brueberg
+// CSC435 Adv Web App Development
+// Want Application - constants.js
+// April 9, 2021
+// Last Edited (Initials, Date, Edits):
+//  DAB, 04/10/2021, Added comments
+//  DAB, 04/18/2021, Added in USER_LOG_IN and 
+// USER_LOG_OUT
+
+// These constants ensure that there will be no 
+// spelling errors when working with reducers.js 
+// and actions.js. They describe the different 
+// action types.
+const constants = {
+    
+}
+
+// Exporting constants
+export default constants;
