f7eb5ade5d07ced28c1863fa4abe3645dccd2540
Devin Brueberg <brueberd@csp.edu>
2022-02-05 07:43:02 -0600

    Half point in loading data/Not complete yet

--- frontend/src/components/views/UserDashboard.js
+++ frontend/src/components/views/UserDashboard.js
@@ -6,17 +6,88 @@
 
 // Using React library in order to build components 
 // for the app and importing needed components
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, ListGroup } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import BodyContainer from '../template/BodyContainer';
+import testData from "../../redux/initialState.json";
 
 function UserDashboard(props) {
+    // Temporary test data 
+    const [data, setData]=useState([]);
+
+    // Temporary test data fetch
+    const getData=()=>{
+        fetch('initialState.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            console.log(response)
+            return response.json();
+          })
+          .then(function(myJson) {
+            console.log(myJson);
+            setData(myJson)
+          });
+      }
+
+      // Temp test data function call
+      useEffect(() => {
+          getData();
+      }, []);
+
+        // const { user } = data.state;
+        // const currentUser = user[0];
+        // const currentAddress = currentUser.address;
+        // const userData = (data && data.length > 0) =>  {}
+        const newData = data.map((testData) => data); 
+        const print = () => console.log(data);
     return (
-        <div className="userDashboard">
+        
+        <BodyContainer>
             <h1>
-                Welcome to the User Dashboard!
+                User Dashboard
             </h1>
+            {print}
+            <Card>
+                <Card.Body>
+                    <Card.Text>
+                        <ListGroup>
+                            
+                            {/* <ListGroup.Item>
+                                {"User Id: " + user.id}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {"First Name: " + currentUser.firstName}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {"LastName: " + currentUser.lastName}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {"Address: " + currentAddress.address}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {"City: " + currentAddress.city}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {"State: " + currentAddress.state}
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                {"Zip: " + currentAddress.zip}
+                            </ListGroup.Item> */}
+                        </ListGroup>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
             <Link to="/">Back to Home</Link>
-        </div>
+        </BodyContainer>
+            
+        
     )
 }
 
