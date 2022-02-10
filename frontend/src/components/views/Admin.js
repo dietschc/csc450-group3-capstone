78f0afdaded9ca960c6bc56822c513c1d753365b
Devin Brueberg <brueberd@csp.edu>
2022-02-10 14:00:40 -0600

    Form Complete/Needs result components

--- frontend/src/components/views/Admin.js
+++ frontend/src/components/views/Admin.js
@@ -6,17 +6,67 @@
 
 // Using React library in order to build components 
 // for the app and importing needed components
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Button, Nav, Form, Container, FormControl, FormGroup, Row, Col, ButtonGroup } from 'react-bootstrap';
+import XLContainer from '../template/XLContainer';
 
 function Admin(props) {
+    const [ searchOption, setSearchOption ] = useState();
+    const searchSubmitHandler= (e) => {
+        e.preventDefault();
+        console.log("FORM SUBMITTED")
+        console.log(e.target.search.value)
+        console.log(e.target.searchOption.value)
+        setSearchOption(e.target.searchOption.value)
+    }
+
+    
     return (
-        <div className="admin">
+        <XLContainer>
             <h1>
-                Welcome to the Admin Page!
+                Admin
             </h1>
-            <Link to="/">Back to Home</Link>
-        </div>
+            <Form onSubmit={searchSubmitHandler}>
+                <Row>
+                    <FormGroup as={Col} sm={8} className="d-flex justify-content-around align-items-center">
+                        <FormControl
+                        type="search"
+                        name="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                        />
+                        <Button type="submit" variant="primary">
+                            Search
+                        </Button>
+                    </FormGroup>
+                    
+                    <ButtonGroup as={Col} sm={4} 
+                    className="d-flex justify-content-around align-items-center" 
+                    name="searchOption" 
+                    onChange={e => console.log(e.target.value)}>
+                        <Form.Check
+                        label="User"
+                        name="searchOption"
+                        type="radio"
+                        value="user"
+                        defaultChecked
+                        id="searchOptionRadio1"
+                        />
+                        <Form.Check
+                        label="Restaurant"
+                        name="searchOption"
+                        type="radio"
+                        value="restaurant"
+                        id="searchOptionRadio1"
+                        />
+                    </ButtonGroup>
+
+                </Row>
+                    </Form>
+                    <Link to="/">Back to Home</Link>
+                </XLContainer>
     )
 }
 
