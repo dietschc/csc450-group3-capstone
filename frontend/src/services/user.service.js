7b17aaec21f90eb97f0c2cc5604f202f6e197ff7
ROMCruise <45038867+dietschc@users.noreply.github.com>
2022-02-17 19:53:34 -0600

    Adding service
    We're going to have to update the actions to use the service class but that is a separate issue.

---
+++ frontend/src/services/user.service.js
@@ -0,0 +1,37 @@
+// Coleman Dietsch
+// CSC450 Capstone
+// Restaurant Club - user.service.js
+// February 14, 2022
+// Last Edited (Initials, Date, Edits):
+
+import http from "../http-common";
+
+class UserDataService {
+
+  // Gets all users
+  getAll() {
+    return http.get("/users");
+  }
+
+  // Gets a specific user
+  get(id) {
+    return http.get(`/users/${id}`);
+  }
+
+  // Creates a new user by calling the adduser method on the users controller
+  create(data) {
+    return http.post("/users/adduser", data);
+  }
+
+  // Updates a specific user
+  update(id, data) {
+    return http.put(`/users/${id}`, data);
+  }
+
+  // Deletes a specific user
+  delete(id) {
+    return http.delete(`/users/${id}`);
+  }
+
+}
+export default new UserDataService();
