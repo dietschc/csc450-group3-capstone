6d07b2d3bc9a2f0be54a1ede9bef129475818f61
Devin Brueberg <brueberd@csp.edu>
2022-02-25 17:00:16 -0600

    create review complete

--- backend/controllers/review.controller.js
+++ backend/controllers/review.controller.js
@@ -22,8 +22,6 @@ exports.create = async (req, res) => {
         return;
     }
 
-    const { restaurantId } = req.body;
-
     // Creating an array to hold the needed table ideas as the adjoining 
     // restaurant tables are created
     const reviewData = {
@@ -35,14 +33,18 @@ exports.create = async (req, res) => {
         historyId: null
     };
 
-    await Restaurant.findByPk(1)
-    .then(async(restaurant) => {
+    // Searching the database to verify a restaurant exists to write the review for
+    await Restaurant.findByPk(reviewData.restaurantId)
+    .then(async (restaurant) => {
+        // If a restaurant was found the rating will be created
         if (restaurant) {
-            // Code for creating a review
+            // A new history entry is added for the rating
             const newHistory = await History.create(req.body)
             .then(history => {
+                // The reviewData array is updated with the historyId
                 reviewData.historyId = history.historyId;
 
+                // The history object is returned to the caller
                 return history;
             })
             .catch(err => {
@@ -53,10 +55,13 @@ exports.create = async (req, res) => {
                 });
             });
 
+            // A new rating is created for the review
             const newRating = await Rating.create(req.body)
             .then(rating => {
+                // The ratingId is updated in the reviewData array
                 reviewData.ratingId = rating.ratingId;
 
+                // Returning the rating query instance
                 return rating;
             })
             .catch(err => {
@@ -67,37 +72,59 @@ exports.create = async (req, res) => {
                 });
             });
 
+            // A new image row in the image table is created for the review
             const newImage = await Image.create(req.body)
             .then(image => {
+                // Returning the image instance to the caller
                 return image;
             })
             .catch(err => {
                 // If there is an error, a response is sent to notify the requester
                 res.status(500).send({
-                    message: err.message || "Some error occurred while creating the Image."
+                    message: 
+                        err.message || "Some error occurred while creating the Image."
                 });
             });
-
             
-
+            // Creating the review table for the new review
             const newReview = await Review.create(reviewData)
             .then(newReview => {
+                // The new review object is returned to the caller
                 return newReview;
-                // res.json({ ...newReview.dataValues, newHistory, newRating, newImage})
             })
             .catch(err => {
                 // If there is an error, a response is sent to notify the requester
                 res.status(500).send({
-                    message:
-                        err.message || "Some error occurred while creating the Review."
+                    // The requester is notified there was an error
+                    message: err.message || "Some error occurred while creating the Review."
                 });
             });
 
+            // Searching for the restaurant rating to update
+            await Rating.findByPk(restaurant.ratingId)
+            .then(async rating => {
+                // Updating the restaurant rating by adding in the new rating to the 
+                // restaurants rating table
+                await rating.update({
+                tasteRating: rating.tasteRating + req.body.tasteRating,
+                serviceRating: rating.serviceRating + req.body.serviceRating,
+                cleanlinessRating: rating.cleanlinessRating + req.body.cleanlinessRating,
+                overallRating: rating.overallRating + req.body.overallRating
+                });
+            
+                // Incrementing the restaurants reviewCount by 1 since there is one new review
+                await restaurant.increment('reviewCount', { by: 1});
+            });
+            
+            // Connecting the review and image tables for the new review image
             await ReviewImage.create({
                 imageId: newImage.imageId,
                 reviewId: newReview.reviewId
             })
-            .then(() => res.json({ ...newReview.dataValues, newHistory, newRating, newImage}))
+            .then(() => {
+                // Sending the newly created review data values back to the requester
+                res.json({ ...newReview.dataValues, newHistory, newRating, newImage})
+            })
             .catch(err => {
                 // If there is an error, a response is sent to notify the requester
                 res.status(500).send({
@@ -105,10 +132,11 @@ exports.create = async (req, res) => {
                         err.message || "Some error occurred while creating the ReviewImage."
                 });
             });
-
         }
+        // Else the restaurant does not exist so a review cannot be created for it
         else {
             res.status(500).send({
+                // Letting the requester know the review could not be created
                 message: `Cannot create a review. No restaurant with id=${restaurantId}.`
             });
         }
