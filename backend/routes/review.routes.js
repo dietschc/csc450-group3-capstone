2edc8464f382e1e96a3bac45e75ba1824cee8ee2
Devin Brueberg <brueberd@gmail.com>
2022-02-27 20:20:57 -0600

    Advance review controllers tested and built

--- backend/routes/review.routes.js
+++ backend/routes/review.routes.js
@@ -1,28 +1,49 @@
 // Initially Created by: Devin Brueberg
 // CSC450 Capstone
-// Restaurant Club - review.routes.js
+// review Club - review.routes.js
 // February 20, 2022
 // Last Edited (Initials, Date, Edits):
 
 module.exports = app => {
-    const review = require("../controllers/review.controller.js");
-    var router = require("express").Router();
+  const review = require("../controllers/review.controller.js");
+  var router = require("express").Router();
 
-    // Create a new Restaurant
-    router.post("/", review.create);
+  // Create a new review
+  router.post("/", review.create);
 
-    // Retrieve all Restaurants
-    router.get("/", review.findAll);
+  // Retrieve all review
+  router.get("/", review.findAll);
 
-    // Retrieve a single Restaurant with id
-    router.get("/:id", review.findOne);
+  // Retrieve a single review with id
+  router.get("/:id", review.findOne);
 
-    // Update a Restaurant with id
-    router.put("/:id", review.update);
+  // Update a review with id
+  router.put("/:id", review.update);
 
-    // Delete a Restaurant with id
-    router.delete("/:id", review.delete);
+  // Delete a review with id
+  router.delete("/:id", review.delete);
 
-    // URL to restaurant for route
-    app.use('/review', router);
+  // Retrieve review matching the author id ordered asc by review name
+  router.get("/author/:id", review.findByAuthorId);
+
+  // Retrieve review  matching the author id ordered asc by review name and result limit
+  router.get("/author/:offset/:limit/:id", review.findByAuthorIdOffsetLimit);
+
+  // Retrieve review ordered asc by review name and result limit
+  router.get("/limit/:limit", review.findAllLimit);
+
+  // Retrieve review ordered asc by review name with result limit and offset
+  router.get("/limit/:offset/:limit", review.findAllOffsetLimit);
+
+  // Retrieve review searched by review name with offset and limit
+  router.get("/search/:offset/:limit/:name", review.findByNameOffsetLimit);
+
+  // Retrieve all Reviews sorted newest to oldest
+  router.get("/sorted/date", review.findAllSortedByDate);
+
+  // Retrieve all Reviews sorted newest to oldest
+  router.get("/sorted/date/:offset/:limit", review.findAllSortedByDateOffsetLimit);
+
+  // URL to review for route
+  app.use('/review', router);
   };
