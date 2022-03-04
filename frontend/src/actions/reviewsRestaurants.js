faab6421b91aa683b0638bbce384bac2efccd149
ROMCruise <45038867+dietschc@users.noreply.github.com>
2022-03-03 22:47:12 -0600

    Updated UserDashboard and created findByAuthorIdThunk

--- frontend/src/actions/reviewsRestaurants.js
+++ frontend/src/actions/reviewsRestaurants.js
@@ -4,6 +4,7 @@
 // March 1, 2022
 // Last Edited (Initials, Date, Edits):
 //  (DAB, 3/02/2022, "Added in some additional comments, cleaned up code")
+//  (CPD, 3/03/2022, "Added in findByAuthorIdThunk")
 
 // Using React library in order to build components 
 // for the app and importing needed components
@@ -28,6 +29,72 @@ export const findAllReviewsRestaurantsOrderedThunk = (offset, limit) => async di
     // Making a call to the database to request the reviews
     await ReviewDataService.findAllOffsetLimit(offset, limit)
     .then(async res => {
+        // If data was found in the database query it is formatted 
+        // for redux and added to state
+        if (res) {
+            // Iterating through the review data
+            await res.data.map(review => {
+                // Formatting the database data so it matches redux
+                const reviewData = formatDBReviewFind(review);
+
+                // Adding the restaurant Id for the current review
+                restaurantIdSet.add(reviewData.restaurantId);
+               
+                // Adding the current review to state
+                dispatch(addReview(reviewData));
+
+                // Returning the current review
+                return review;
+            })
+        }
+    })
+    .catch(err => {
+        // If there was an error it is logged in the console
+        console.log(err)
+    })
+
+    // If there are restaurant id's in the set the database is queried to return all matching
+    // restaurant data
+    restaurantIdSet.size > 0 && await RestaurantDataService.findAllByArray(Array.from(restaurantIdSet))
+    .then(async res => {
+        // If there is data in the query it is added to redux state
+        if (res) {
+            // Iterating through the restaurant data
+            await res.data.map(restaurant => {
+                // The restaurant data is formatted to be added to redux state
+                const restaurantData = formatDBRestaurantFind(restaurant);
+
+                // Adding the restaurant to redux state
+                dispatch(addRestaurant(restaurantData));
+
+                // Returning the restaurant data
+                return restaurant;
+            })
+        }
+    })
+    .catch(err => {
+        // If there was an error it is logged in the console
+        console.log(err);
+    })
+}
+
+/**
+ * Adds the specified offset and limit of reviews with their id referenced restaurants to 
+ * their respective Redux states. 
+ * 
+ * @param {*} offset 
+ * @param {*} limit 
+ * @returns 
+ */
+ export const findByAuthorIdThunk = (userId) => async dispatch => {
+    // Creating a Set to hold unique restaurant id's
+    const restaurantIdSet = new Set();
+    
+    // Making a call to the database to request the reviews
+    await ReviewDataService.findByAuthorId(userId)
+    .then(async res => {
+        console.log("returned values: ", res);
+
         // If data was found in the database query it is formatted 
         // for redux and added to state
         if (res) {
--- frontend/src/components/views/UserDashboard.js
+++ frontend/src/components/views/UserDashboard.js
@@ -10,13 +10,14 @@
 //  (DAB, 02/06/2022, breaking up components/functionality into their own .js files)
 //  (DAB, 02/07/2022, changed buttons to buttonGroup function)
 //  (DAB, 02/12/2022, Refactored variables to match altered JSON array)
-//  (CPD, 03/02/2022, Wiring up frontend to use parameters from backend)
+//  (CPD, 03/02/2022, Wiring up frontend to use parameters from backend (user, authentication, address))
+//  (CPD, 03/03/2022, Got currentUser info including friends, reviews, and restaurants loading from backend)
 
 // Using React library in order to build components 
 // for the app and importing needed components
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import mockStateData from "../../redux/initialState.json";
+// import mockStateData from "../../redux/initialState.json";
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import FriendList from '../subComponent/FriendList';
@@ -24,23 +25,43 @@ import UserInfo from '../subComponent/UserInfo';
 import RestaurantReviewDetail from '../subComponent/RestaurantReviewDetail';
 import UDRestaurantReviewDetail from '../form/button/UDRestaurantReviewDetail';
 import DeleteReviewConfirm from '../modal/DeleteReviewConfirm';
+import { findByAuthorIdThunk } from '../../actions/reviewsRestaurants';
+import { deleteAllReviews } from '../../actions/reviews';
+import { deleteAllRestaurants } from '../../actions/restaurants';
 
 function UserDashboard(props) {
-    const { users } = props;
+    const {
+        users,
+        restaurants,
+        reviews,
+        findByAuthorIdThunk,
+        deleteAllReviews,
+        deleteAllRestaurants,
+    } = props;
 
 
     // *** Temporary test data, this will be replaced with Redux in the future ***
-    const [data, setData]=useState(mockStateData);
+    // const [data, setData] = useState(mockStateData);
     const [showFriendConfirm, setShowFriendConfirm] = useState(false);
     const [showReviewConfirm, setShowReviewConfirm] = useState(false);
     const [friend, setFriend] = useState([]);
     const [currentReview, setCurrentReview] = useState([]);
 
     // Destructuring the needed data from the intitialState.json file
-    const { restaurants, reviews, messages } = data; 
+    // const { messages } = data;
     const [user] = users;
-    const { address: currentAddress }  = user;
+    const { address: currentAddress } = user;
     const { friends } = user;
+
+    const loadState = () => {
+        deleteAllReviews();
+        deleteAllRestaurants();
+        findByAuthorIdThunk(user.id);
+    }
+
+    useEffect(() => {
+        loadState();
+    }, []);
 
     // Allows for the navigation to the specified webpage
     const navigate = useNavigate();
@@ -59,10 +80,10 @@ function UserDashboard(props) {
 
     // Deletes the friend with the returned friend.userId
     const deleteFriend = () => {
-            // Delete Friend Code Here, use friend.userId to 
-            // grab the correct friend
-            console.log(friend.userName + " was deleted!");
-        }
+        // Delete Friend Code Here, use friend.userId to 
+        // grab the correct friend
+        console.log(friend.userName + " was deleted!");
+    }
 
     // Handlers for the DeleteReviewConfirm modal
     const showReviewHandler = () => setShowReviewConfirm(true);
@@ -107,21 +128,21 @@ function UserDashboard(props) {
     // The review variable needs to be passed as an argument so it can 
     // be generated in the parent
     const buttonGroup = (review) => (
-        <UDRestaurantReviewDetail reviewEditHandler={reviewEditHandler} 
-        deleteReviewHandler={deleteReviewHandler} review={review}/>
+        <UDRestaurantReviewDetail reviewEditHandler={reviewEditHandler}
+            deleteReviewHandler={deleteReviewHandler} review={review} />
     )
 
     // This modal will be passed into the RestaurantReviewDetail.js 
     // Component to allow a confirmation before a review is deleted
     const deleteButtonModal = (
-        <DeleteReviewConfirm 
-        show={showReviewConfirm} 
-        deleteReview={deleteReview}
-        closeHandler={closeReviewHandler} />
+        <DeleteReviewConfirm
+            show={showReviewConfirm}
+            deleteReview={deleteReview}
+            closeHandler={closeReviewHandler} />
     )
 
     return (
-        <Container className="justify-content-center" style={{maxWidth: "1000px"}}>
+        <Container className="justify-content-center" style={{ maxWidth: "1000px" }}>
             {/* {console.log("Test is ", users)}
             {console.log("Current User is ", user)}
             {console.log("Current Address is ", currentAddress)}
@@ -131,21 +152,21 @@ function UserDashboard(props) {
             </h1>
             <Row>
                 <Col className="pb-2" md={6}>
-                    <UserInfo 
-                    currentUser={user}   
-                    currentAddress={currentAddress} 
-                    userInfoHandler={userInfoHandler}/>
+                    <UserInfo
+                        currentUser={user}
+                        currentAddress={currentAddress}
+                        userInfoHandler={userInfoHandler} />
                 </Col>
                 <Col className="pb-2" md={6}>
-                    <FriendList friend={friend} chatHandler={chatHandler} 
-                    deleteFriendHandler={deleteFriendHandler} showFriendConfirm={showFriendConfirm} 
-                    deleteFriend={deleteFriend} closeFriendHandler={closeFriendHandler}
-                    friends={friends} closeReviewHandler={closeReviewHandler} 
-                    restaurant={restaurants}/>
+                    <FriendList friend={friend} chatHandler={chatHandler}
+                        deleteFriendHandler={deleteFriendHandler} showFriendConfirm={showFriendConfirm}
+                        deleteFriend={deleteFriend} closeFriendHandler={closeFriendHandler}
+                        friends={friends} closeReviewHandler={closeReviewHandler}
+                        restaurant={restaurants} />
                 </Col>
             </Row>
-            <RestaurantReviewDetail reviews={reviews} restaurants={restaurants} 
-            buttonGroup={buttonGroup} modal={deleteButtonModal}/>
+            <RestaurantReviewDetail reviews={reviews} restaurants={restaurants}
+                buttonGroup={buttonGroup} modal={deleteButtonModal} />
         </Container>
     )
 }
@@ -153,9 +174,12 @@ function UserDashboard(props) {
 // Mapping the redux store states to props
 const mapStateToProps = state =>
 ({
-    users: [...state.users]
+    users: [...state.users],
+    restaurants: [...state.restaurants],
+    reviews: [...state.reviews]
 });
 
 // Exporting the component
 // export default UserDashboard;
-export default connect(mapStateToProps, {})(UserDashboard);
+export default connect(mapStateToProps, { 
+    findByAuthorIdThunk, deleteAllRestaurants, deleteAllReviews })(UserDashboard);
