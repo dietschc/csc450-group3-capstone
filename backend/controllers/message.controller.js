f39eb4a022609e57aca92aad47c8354f66521c04
Devin Brueberg <brueberd@gmail.com>
2022-03-14 16:13:02 -0500

    Chat milestone 2 fully functional using interval/Comments are added

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -4,6 +4,8 @@
 // February 27, 2022
 // Last Edited (Initials, Date, Edits):
 //  (DAB, 2/28/2022, Added in comments)
+//  (DAB, 3/14/2022, Added findAllAfterDateOffsetLimit method to 
+//  query all post createdAt times)
 
 const db = require("../models");
 const { Op } = db.Sequelize;
@@ -31,11 +33,13 @@ exports.create = async (req, res) => {
     }
 
     // Searching if the conversation between the two users exists or not
-    await Conversation.findOne({ 
-        where: {[Op.and]: [ 
-            { userToId: req.body.userToId }, 
-            { userFromId: req.body.userFromId } 
-        ]}
+    await Conversation.findOne({
+        where: {
+            [Op.and]: [
+                { userToId: req.body.userToId },
+                { userFromId: req.body.userFromId }
+            ]
+        }
     }).then(async conversation => {
         // If the conversation exists, a mew message is added to the database
         if (conversation) {
@@ -44,34 +48,34 @@ exports.create = async (req, res) => {
 
             // Creating the new message and returning the values created
             await Message.create(messageData)
-            .then(message => {
-                // Returning the message data to the caller
-                res.send({ ...message.dataValues, conversation });
-            });
+                .then(message => {
+                    // Returning the message data to the caller
+                    res.send({ ...message.dataValues, conversation });
+                });
         }
         // Else there is not a conversation entry in the database so a new 
         // one is created before the message is added
         else {
             // Creating a new conversation with the message details
             conversation = await Conversation.create(req.body)
-            .then(newConversation => {
-                // Assigning the correct conversationId to the message data
-                messageData.conversationId = newConversation.conversationId;
+                .then(newConversation => {
+                    // Assigning the correct conversationId to the message data
+                    messageData.conversationId = newConversation.conversationId;
 
-                // Returning that conversation data
-                return newConversation;
-            })
-            .catch(err => {
-                // If there is an error it is logged
-                console.log(err);
-            });
+                    // Returning that conversation data
+                    return newConversation;
+                })
+                .catch(err => {
+                    // If there is an error it is logged
+                    console.log(err);
+                });
 
             // Creating the message in the database with the message data
             await Message.create(messageData)
-            .then(message => {
-                // Returning the message data to the caller
-                res.send({ ...message.dataValues, conversation });
-            })
+                .then(message => {
+                    // Returning the message data to the caller
+                    res.send({ ...message.dataValues, conversation });
+                })
         }
     }).catch(err => {
         // If there is an error, it is send back to the requester
@@ -87,7 +91,7 @@ exports.create = async (req, res) => {
 exports.findAll = async (req, res) => {
     // Searching for all messages and their attached conversation entires
     await Message.findAll({
-        include: [ Conversation ]
+        include: [Conversation]
     })
         .then(message => {
             // If found, sending them back to the requester
@@ -110,27 +114,27 @@ exports.findOne = async (req, res) => {
 
     // Searching for a message and its attached conversation entry by message Id
     await Message.findByPk(messageId, {
-        include: [ Conversation ]
+        include: [Conversation]
     })
-    .then(message => {
-        // If a message was found the result is sent back
-        if (message) {
-            res.send(message);
-        }
-        // If a message was not found the user is notified
-        else {
-            res.send({
-                message: `No messages found with message id ${messageId}!`
+        .then(message => {
+            // If a message was found the result is sent back
+            if (message) {
+                res.send(message);
+            }
+            // If a message was not found the user is notified
+            else {
+                res.send({
+                    message: `No messages found with message id ${messageId}!`
+                });
+            }
+        })
+        .catch(err => {
+            // If there was an error the requester is notified
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving messages."
             });
-        }
-    })
-    .catch(err => {
-        // If there was an error the requester is notified
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while retrieving messages."
         });
-    });
 };
 
 
@@ -166,32 +170,103 @@ exports.findByConversationIdOffsetLimit = async (req, res) => {
     // limit will retrieve the correct message order
     await Message.findAll({
         subQuery: false,
-        include: [ Conversation ],
-        order: [['updatedAt', 'ASC']],
-        where: {[Op.or]: [ 
-            {
-                [Op.and]: [
-                    { '$conversation.userToId$': userToId }, 
-                    { '$conversation.userFromId$': userFromId }
-            ]},
-            {
-                [Op.and]: [
-                    { '$conversation.userToId$': userFromId }, 
-                    { '$conversation.userFromId$': userToId } 
-            ]}
-        ]},
-        offset: searchOffset, 
-        limit: searchLimit 
+        include: [Conversation],
+        order: [['updatedAt', 'DESC'], ['messageId']],
+        where: {
+            [Op.or]: [
+                {
+                    [Op.and]: [
+                        { '$conversation.userToId$': userToId },
+                        { '$conversation.userFromId$': userFromId }
+                    ]
+                },
+                {
+                    [Op.and]: [
+                        { '$conversation.userToId$': userFromId },
+                        { '$conversation.userFromId$': userToId }
+                    ]
+                }
+            ]
+        },
+        offset: searchOffset,
+        limit: searchLimit
     })
-    .then(message => {
-        // If messages are found they are sent back to the requester
-        res.send(message);
-    })
-    .catch(err => {
-        // If there is an error the requester will be notified
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while retrieving messages."
+        .then(message => {
+            // If messages are found they are sent back to the requester
+            res.send(message);
+        })
+        .catch(err => {
+            // If there is an error the requester will be notified
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving messages."
+            });
         });
-    });
+}
+
+/**
+ * The findAllAfterDateOffsetLimit method will find all messages using the user and friend 
+ * ids created after the createBy data up to the specified limit and offset.
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.findAllAfterDateOffsetLimit = async (req, res) => {
+    // Checking that offset and limit are numbers, if not a default value will be used
+    const searchOffset = isNaN(req.params.offset) ? 0 : parseInt(req.params.offset);
+    const searchLimit = isNaN(req.params.limit) ? 999999999999 : parseInt(req.params.limit);
+    // Grabbing the ids and createdAt date out of params to find the conversation
+    const { userToId, userFromId, createdAt } = req.params;
+
+    // Async searching the database and returning all messages. The 
+    // search includes all joined tables and attributes. Data will 
+    // be sorted by updatedAt time and in DESC order so that offset and 
+    // limit will retrieve the correct message order
+    await Message.findAll({
+        subQuery: false,
+        include: [Conversation],
+        order: [['updatedAt', 'DESC']],
+        where: {
+            [Op.and]: [
+                {
+                    [Op.or]: [
+                        {
+                            [Op.and]: [
+                                { '$conversation.userToId$': userToId },
+                                { '$conversation.userFromId$': userFromId }
+                            ]
+                        },
+                        {
+                            [Op.and]: [
+                                { '$conversation.userToId$': userFromId },
+                                { '$conversation.userFromId$': userToId }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    createdAt: { [Op.gt]: createdAt }
+                }]
+        },
+        offset: searchOffset,
+        limit: searchLimit
+    })
+        .then(message => {
+            if (message) {
+                // If messages are found they are sent back to the requester
+                res.send(message);
+            }
+            else {
+                // If nothing is found a 404 will be sent back
+                res.status(404).send({ message: "No new messages"})
+            }
+            
+        })
+        .catch(err => {
+            // If there is an error the requester will be notified
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving messages."
+            });
+        });
 }
--- frontend/src/components/views/Chat.js
+++ frontend/src/components/views/Chat.js
@@ -4,6 +4,9 @@
 // January 24, 2022
 // Last Edited (Initials, Date, Edits):
 //  (DAB, 02/12/2022, Refactored variables to match altered JSON array)
+//  (DAB, 03/14/2022, Chat box now scrolls to the bottom after each message)
+//  (DAB, 03/14/2022, Chat now works with interval chat on 15 sec delay)
+//  (DAB, 03/14/2022, Added comments)
 
 // Using React library in order to build components 
 // for the app and importing needed components
@@ -15,123 +18,146 @@ import { connect } from 'react-redux';
 import { formatTimeCalendar } from '../../helperFunction/FormatString';
 import {
     findByConversationIdOffsetLimitThunk,
+    findAllAfterDateOffsetLimitThunk,
     deleteAllMessages,
     sendMessageThunk
 } from '../../actions/messages';
 
+/**
+ * The Chat View will allow friends to talk to one another via text 
+ * based chat.
+ * 
+ * @param {*} props 
+ * @returns 
+ */
 function Chat(props) {
     const {
         messages,
         users,
         deleteAllMessages,
         findByConversationIdOffsetLimitThunk,
+        findAllAfterDateOffsetLimitThunk,
         sendMessageThunk
     } = props;
 
+    // The chatMessage will hold the state of the message text area
     const [chatMessage, setChatMessage] = useState("");
 
     // Extract friend ID from parameters
     const { id: friendId } = useParams();
-
     // Extract current user from users state array
-    const [user = []] = users;
-
+    const [user] = users;
     // Create a copy of the friends array
-    const friends = user.friends;
-
+    const friends = user?.friends;
     // Get friend specified in parameter
     const [paramFriend] = friends ? friends.filter((friend) => (friend.id) === Number(friendId)) : [];
-
-    const messageScrollTo = useRef();
-
-    // Convenience variables
+    // Convenience variables to reference current state
     const userName = user?.auth?.userName;
     const friendName = paramFriend?.userName;
 
-    const loadState = () => {
+    // This const references the end of the message body
+    const messageScrollTo = useRef();
+    
+    // The loadState method will load the initial Chat state from the database
+    const loadState = async () => {
+        // All old messages are deleted
         deleteAllMessages();
-        findByConversationIdOffsetLimitThunk(user.id, friendId);
+
+        // New messages are queried from the database
+        await findByConversationIdOffsetLimitThunk(user.id, friendId, 0, 15);
     }
 
-    // The use effect will rerender only once initially
-    useEffect(() => {
-        loadState();
-        
+    // The database is queried for new messages that may exist in the database, 
+    // the state is only updated if data is found
+    const loadNewMessages = () => {
+        // createdAt is primed to be 0 so all new messages can be retrieved
+        let createdAt = 0;
 
-        // const interval = setInterval(() => {
-        //     console.log('This will run every 15 seconds!');
-        //     loadState();
-        // }, 15000);
-        // return () => clearInterval(interval);
+        // If there are messages in state the newest messages data will be assigned 
+        // to createdBy for the database query
+        if (messages.length > 0) {
+            // The last message in the message array is destructured out
+            const [oldestMessage] = [...messages].reverse();
+
+            // createdAt is set to the newest messages createdAt time
+            createdAt = oldestMessage.timeStamp;
+        }
+
+        // The database is checked for new messages
+        findAllAfterDateOffsetLimitThunk(createdAt, user.id, friendId, 0, 15)
+    }
+
+    // The use effect will rerender only once initially and will only load state 
+    // if a user is logged in
+    useEffect(() => {
+        if (user?.id) {
+            loadState();
+        }
     }, []);
 
-    // The use effect will rerender only when messages state has changed
+    // The use effect will rerender only when messages state has changed. It 
+    // will scroll to the end of the message box and trigger a database query for 
+    // new messages on an interval timer
     useEffect(() => {
+        // Scrolling to the last message
         messageEndScroll()
+        
+        // If a user is logged in and has an id an interval will be set to query 
+        // the database for new messages
+        if (user?.id) {
+            const interval = setInterval(async () => {
+                // Calling loadNewMessages to check and possibly load in new messages
+                await loadNewMessages();
+            }, 15000);
+
+            // The interval is cleared when the component is unmounted
+            return () => clearInterval(interval);
+        }
     }, [messages]);
 
-    
-
-    const onChangeMessage = e => {
+    // This function will set chatMessage state with the new message
+    const onChangeMessage = (e) => {
         const chatMessage = e.target.value
         setChatMessage(chatMessage);
     }
 
-    // The moreHandler will load in the Search View with the 
-    // needed URL parameters for the desired search
+    // The sendMessageHandler will sent the message to be added to both the database 
+    // and state
     const sendMessageHandler = (e) => {
+        // Preventing default form action
         e.preventDefault();
-        // console.log(chatMessage);
 
+        // Easy to understand variables to be used in adding the message to memory
         const userToId = friendId;
-        const userFromId = user.id;
+        const userFromId = user?.id;
         const message = chatMessage;
 
-        // Call thunk and pass message parameters
-        sendMessageThunk(userToId, userFromId, message);
-
+        // If there is no text in the chatMessage it will not be added to the database or state
+        if (message) {
+            // Call thunk and pass message parameters
+            user?.id && sendMessageThunk(userToId, userFromId, message);
+        }
+        
         // Clear form after you send each message
         clearForm();
-
-        // Scrolling down to the newest message when one is sent
-        
-        // messageEndScroll()
-        // const testState = {
-        //     messageId: 0,
-        //     toUserId: 1,
-        //     fromUserId: 2,
-        //     message: "Test message"
-        // }
-        // addMessage(testState.toUserId, testState.fromUserId, testState.message)
-        // deleteMessage(testState.messageId)
-        // deleteAllMessages()
-        // setMessageHistory( 
-        //     ...messages,
-        //     {
-        //         messageId: 10,
-        //         userMessage: {
-        //             userMessageId: 4,
-        //             to: 1,
-        //             from: 0
-        //         },
-        //         message: {chatMessage},
-        //         timeStamp: Date.toLocaleString()
-        //     }
-        // );
-        // setChatMessage("");
-        // console.log(messages)
     }
 
+    // Clears the chatMessage text input after each new chat message
     const clearForm = () => {
         setChatMessage("");
-        // console.log("Form cleared")
     }
 
+    // This function will scroll the message window to the bottom to read the newest message
     const messageEndScroll = () => {
+        // If the page is not rendered nothing is done
         if (!messageScrollTo.current) return;
+
+        // If the page is rendered the message box will be scrolled to the bottom
         messageScrollTo.current.scrollIntoView({ behavior: 'smooth', block: 'end', inline: 'nearest'});
     }
 
+    // The formatMessages method will format all the new messages to display based off to or from users. 
+    // The result will be an easy to read text display
     const formatMessages = () => (
         <>
             {messages.map((message, index) => (
@@ -153,6 +179,7 @@ function Chat(props) {
         </>
     )
 
+    // Returning the Chat View
     return (
         <FormContainer>
             <h1>
@@ -167,7 +194,7 @@ function Chat(props) {
                     <span className="p-0 m-0" ref={messageScrollTo} />
                 </Card.Text>
                 <Card.Body>
-                    <Form className="">
+                    <Form onSubmit={sendMessageHandler} className="">
                         <Row>
                             <Col></Col>
                             <Col sm={12} md={6}>
@@ -180,15 +207,13 @@ function Chat(props) {
                                 </Form.Group>
                                 <Button type="submit"
                                     className="d-flex ms-auto justify-content-center"
-                                    style={{ width: "5rem" }}
-                                    onClick={(e) => sendMessageHandler(e)}>Send</Button>
+                                    style={{ width: "5rem" }}>Send</Button>
                             </Col>
                         </Row>
                     </Form>
                 </Card.Body>
             </Card>
         </FormContainer>
-
     )
 }
 
@@ -200,27 +225,11 @@ const mapStateToProps = state =>
     messages: [...state.messages]
 });
 
-// Mapping the state actions to props
-// const mapDispatchToProps = dispatch => 
-//     ({
-//         // This method will add a new review
-//         addMessage(toUserId, fromUserId, message) {
-//             dispatch(addMessage(toUserId, fromUserId, message)
-//                 )
-//         },
-//         deleteAllMessages() {
-//             dispatch(deleteAllMessages()
-//             )
-//         },
-//         deleteMessage(id) {
-//             dispatch(deleteMessage(id))
-//         }
-//     })
 
-
-// Exporting the connect Wrapped EditAccount Component
+// Exporting the connect Wrapped Chat Component
 export default connect(mapStateToProps, {
     findByConversationIdOffsetLimitThunk,
+    findAllAfterDateOffsetLimitThunk,
     deleteAllMessages,
     sendMessageThunk
 })(Chat);
