1511b37ec0e14e7fbb4774eabb94cce7d23a2cba
dietschc <dietschc@csp.edu>
2022-03-26 13:40:27 -0500

    Updating expiration values

--- backend/config/auth.config.js
+++ backend/config/auth.config.js
@@ -8,9 +8,12 @@
 // Access token and Refresh Access token expiration settings
 module.exports = {
     secret: "super-secret-key",
-    // jwtExpiration: 3600,           // 1 hour 
-    // jwtRefreshExpiration: 86400,   // 24 hours
-    /* for test */
-    jwtExpiration: 60,          // 1 minute 
-    jwtRefreshExpiration: 120,  // 2 minutes
+    // Expiration of access tokens
+    jwtExpiration: 7200,           // 2 hours
+    // Expiration of refresh tokens
+    jwtRefreshExpiration: 28800,   // 8 hours
+
+    /* For testing */
+    // jwtExpiration: 60,          // 1 minute 
+    // jwtRefreshExpiration: 120,  // 2 minutes
 };
