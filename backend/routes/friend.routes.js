7a4de298b1a0300908cd466c69a9d92b68a4f47c
dietschc <dietschc@csp.edu>
2022-04-04 16:35:56 -0500

    Think I got everything

--- backend/routes/friend.routes.js
+++ backend/routes/friend.routes.js
@@ -1,28 +1,54 @@
 // Initially Created by: Devin Brueberg
 // CSC450 Capstone
-// Restaurant Club - friend.routes.js
+// Friend Club - friend.routes.js
 // March 3, 2022
 // Last Edited (Initials, Date, Edits):
 
 module.exports = app => {
-    const friend = require("../controllers/friend.controller.js");
-    var router = require("express").Router();
+  const friend = require("../controllers/friend.controller.js");
+  var router = require("express").Router();
 
-    // Create a new Restaurant
-    router.post("/", friend.create);
+  const { authJwt } = require("../middleware");
 
-    // Retrieve all Restaurants
-    router.get("/", friend.findAll);
+  // Add access tokens to headers
+  router.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
 
-    // Retrieve a single Restaurant with id
-    router.get("/:id", friend.findOne);
+  // Create a new Friend
+  router.post("/",
+    [authJwt.verifyToken],
+    friend.create
+  );
 
-    // Update a Restaurant with id
-    router.put("/:id", friend.update);
+  // Retrieve all Friends
+  router.get("/",
+    [authJwt.verifyToken],
+    friend.findAll
+  );
 
-    // Delete a Restaurant with id
-    router.delete("/:id/:friendId", friend.delete);
+  // Retrieve a single Friend with id
+  router.get("/:id",
+    [authJwt.verifyToken],
+    friend.findOne
+  );
 
-    // URL to restaurant for route
-    app.use('/friend', router);
-  };
+  // Update a Friend with id
+  router.put("/:id",
+    [authJwt.verifyToken],
+    friend.update
+  );
+
+  // Delete a Friend with id
+  router.delete("/:id/:friendId",
+    [authJwt.verifyToken],
+    friend.delete
+  );
+
+  // URL to restaurant for route
+  app.use('/friend', router);
+};
