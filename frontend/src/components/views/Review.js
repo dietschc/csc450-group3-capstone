b6a46e674e932f7b615009c4d7f35ae540c73966
Devin Brueberg <brueberd@gmail.com>
2022-04-16 23:29:42 -0500

    Max width of disabled star inputs raised by .35rem, cutting off on Galaxy S20 real device

--- frontend/src/components/views/Review.js
+++ frontend/src/components/views/Review.js
@@ -25,6 +25,7 @@
 //  (DAB, 04/14/2022, added endLoadingAll action to page load in to clean 
 //  up any skipped load ins)
 //  (DAB, 04/16/2022, Error console.log removed)
+//  (DAB, 04/16/2022, Adjusted maxWidth of disabled star inputs)
 
 // Using React library in order to build components
 // for the app and importing needed components
@@ -438,7 +439,7 @@ function Review(props) {
                 <Form className="p-0 m-0" onSubmit={handleSubmit}>
                     <div className="d-flex flex-column flex-sm-row p-0 m-0">
                         <div className="d-flex flex-row flex-fill mb-2">
-                            <div className="d-flex flex-fill m-1">
+                            <div className="d-flex flex-fill m-sm-1">
                                 <Form.Group className="d-flex flex-column justify-content-between flex-fill">
                                     <div>
                                         <Form.Label>Taste</Form.Label>
@@ -495,7 +496,7 @@ function Review(props) {
 
                             <div
                                 className="d-flex flex-shrink-0 flex-column justify-content-between m-1"
-                                style={{ maxWidth: "5.65rem" }}
+                                style={{ maxWidth: "6rem" }}
                             >
                                 <Form.Floating className="mb-1 p-0">
                                     <FloatingLabel
